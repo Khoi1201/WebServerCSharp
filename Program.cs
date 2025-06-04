bb8206d65027f79e37503019f319d9cdd5905aa2
ngmin <ngmin@KHOI-DELL-LAP>
2025-06-04 10:35:30 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace WebServerCSharp
+{
+    /// <summary>
+    /// A lean and mean server
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebServer.Start();
+            Console.ReadLine();
+        }
+    }
+}
+
