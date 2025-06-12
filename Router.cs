32e60851fe108ad713582a175d8711a291fff2b4
Khoi Cao <ngminhkhoi1203@gmail.com>
2025-06-12 13:41:24 +0700

    update: add route handler, redirect page, add way to handle POST method, add error handler for file not found, page not found

--- Router.cs
+++ Router.cs
@@ -9,15 +9,21 @@ using WebServerCSharp.Extensions;
 
 namespace WebServerCSharp
 {
-    internal class Router
+  internal class Router
+  {
+    public string WebsitePath { get; set; }
+
+    private Dictionary<string, ExtensionInfo> extFolderMap;
+    private List<Route> routes;
+    public const string POST = "post";
+    public const string GET = "get";
+    public const string PUT = "put";
+    public const string DELETE = "delete";
+
+    public Router()
     {
-        public string WebsitePath { get; set; }
-
-        private Dictionary<string, ExtensionInfo> extFolderMap;
-
-        public Router()
-        {
-            extFolderMap = new Dictionary<string, ExtensionInfo>()
+      routes = new List<Route>();
+      extFolderMap = new Dictionary<string, ExtensionInfo>()
             {
               {"ico", new ExtensionInfo() {Loader=ImageLoader, ContentType="image/ico"}},
               {"png", new ExtensionInfo() {Loader=ImageLoader, ContentType="image/png"}},
@@ -29,77 +35,132 @@ namespace WebServerCSharp
               {"js", new ExtensionInfo() {Loader=FileLoader, ContentType="text/javascript"}},
               {"", new ExtensionInfo() {Loader=PageLoader, ContentType="text/html"}},
             };
-        }
-        /// <summary>
-        /// Read in an image file and returns a ResponsePacket with the raw data.
-        /// </summary>
-        private ResponsePacket ImageLoader(string fullPath, string ext, ExtensionInfo extInfo)
-        {
-            FileStream fStream = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
-            BinaryReader br = new BinaryReader(fStream);
-            ResponsePacket ret = new ResponsePacket() { Data = br.ReadBytes((int)fStream.Length), ContentType = extInfo.ContentType };
-            br.Close();
-            fStream.Close();
-
-            return ret;
-        }
-        /// <summary>
-        /// Read in what is basically a text file and return a ResponsePacket with the text UTF8 encoded.
-        /// </summary>
-        private ResponsePacket FileLoader(string fullPath, string ext, ExtensionInfo extInfo)
-        {
-            string text = File.ReadAllText(fullPath);
-            ResponsePacket ret = new() { Data = Encoding.UTF8.GetBytes(text), ContentType = extInfo.ContentType, Encoding = Encoding.UTF8 };
-
-            return ret;
-        }
-        /// <summary>
-        /// Load an HTML file, taking into account missing extensions and a file-less IP/domain, 
-        /// which should default to index.html.
-        /// </summary>
-        private ResponsePacket PageLoader(string fullPath, string ext, ExtensionInfo extInfo)
-        {
-            ResponsePacket ret = new();
-
-            if (fullPath == WebsitePath) // If nothing follows the domain name or IP, then default to loading index.html.
-            {
-                ret = Route("GET", "/index.html", null);
-            }
-            else
-            {
-                if (string.IsNullOrEmpty(ext))
-                {
-                    // No extension, so we make it ".html"
-                    fullPath = fullPath + ".html";
-                }
-
-                // Inject the "Pages" folder into the path
-                fullPath = WebsitePath + "\\Pages" + fullPath.RightOf(WebsitePath);
-                ret = FileLoader(fullPath, ext, extInfo);
-            }
-
-            return ret;
-        }
-        public ResponsePacket Route(string verb, string path, Dictionary<string, string> kvParams)
-        {
-            string ext = path.RightOfRightmostOf('.');
-            ExtensionInfo extInfo;
-            ResponsePacket ret = null;
-
-            if (extFolderMap.TryGetValue(ext, out extInfo))
-            {
-                // Strip off leading '/' and reformat as with windows path separator.
-                string fullPath = Path.Combine(WebsitePath, path.RightOf('/'));
-                fullPath = fullPath.Replace("/", "\\");
-                ret = extInfo.Loader(fullPath, ext, extInfo);
-            }
-            else
-            {
-                ret = new ResponsePacket() { Error = WebServer.ServerError.UnknownType };
-            }
-
-            return ret;
-        }
-
     }
+    /// <summary>
+    /// Read in an image file and returns a ResponsePacket with the raw data.
+    /// </summary>
+    private ResponsePacket ImageLoader(string fullPath, string ext, ExtensionInfo extInfo)
+    {
+      ResponsePacket ret;
+      if (File.Exists(fullPath))
+      {
+      FileStream fStream = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+      BinaryReader br = new BinaryReader(fStream);
+      ret = new ResponsePacket() { Data = br.ReadBytes((int)fStream.Length), ContentType = extInfo.ContentType };
+      br.Close();
+      fStream.Close();
+      } 
+      else
+      {
+        ret = new ResponsePacket() { Error = WebServer.ServerError.FileNotFound};
+      }
+
+      return ret;
+    }
+    /// <summary>
+    /// Read in what is basically a text file and return a ResponsePacket with the text UTF8 encoded.
+    /// </summary>
+    private ResponsePacket FileLoader(string fullPath, string ext, ExtensionInfo extInfo)
+    {
+      ResponsePacket ret;
+
+      if (File.Exists(fullPath))
+      {
+        string text = File.ReadAllText(fullPath);
+        ret = new() { Data = Encoding.UTF8.GetBytes(text), ContentType = extInfo.ContentType, Encoding = Encoding.UTF8 };
+      }
+      else
+      {
+        ret = new ResponsePacket() { Error = WebServer.ServerError.FileNotFound };
+        Console.WriteLine("File Not Found " + fullPath);
+      }
+
+      return ret;
+    }
+    /// <summary>
+    /// Load an HTML file, taking into account missing extensions and a file-less IP/domain, 
+    /// which should default to index.html.
+    /// </summary>
+    private ResponsePacket PageLoader(string fullPath, string ext, ExtensionInfo extInfo)
+    {
+      ResponsePacket ret = new();
+
+      if (fullPath == WebsitePath) // If nothing follows the domain name or IP, then default to loading index.html.
+      {
+        ret = Route("GET", "/index.html", null);
+      }
+      else
+      {
+        if (string.IsNullOrEmpty(ext))
+        {
+          // No extension, so we make it ".html"
+          fullPath = fullPath + ".html";
+        }
+
+        // Inject the "Pages" folder into the path
+        fullPath = WebsitePath + "\\Pages" + fullPath.RightOf(WebsitePath);
+        if (File.Exists(fullPath))
+        {
+          ret = FileLoader(fullPath, ext, extInfo);
+        } 
+        else
+        {
+          ret = new ResponsePacket() { Error=WebServer.ServerError.PageNotFound};
+        }
+      }
+
+      return ret;
+    }
+    public void AddRoute(Route route)
+    {
+      routes.Add(route);
+    }
+    public ResponsePacket Route(string verb, string path, Dictionary<string, string> kvParams)
+    {
+      string ext = path.RightOfRightmostOf('.');
+      ExtensionInfo extInfo;
+      ResponsePacket ret = null;
+      verb = verb.ToLower();
+      path = path.ToLower();
+
+
+      if (extFolderMap.TryGetValue(ext, out extInfo))
+      {
+        // Strip off leading '/' and reformat as with windows path separator.
+        string fullPath = Path.Combine(WebsitePath, path.RightOf('/'));
+        fullPath = fullPath.Replace("/", "\\");
+
+        Route routeHandler = routes.SingleOrDefault(r => r.Path == path && r.Verb.ToLower() == verb);  
+
+        if (routeHandler != null)
+        {
+          // Application has a handler for this route.
+          string redirect = routeHandler.Action(kvParams);
+
+          if (String.IsNullOrEmpty(redirect))
+          {
+            // Respond with default content loader.
+            ret = extInfo.Loader(fullPath, ext, extInfo);
+          }
+          else
+          {
+            // Respond with redirect.
+            ret = new ResponsePacket() { Redirect = redirect };
+          }
+        }
+        else
+        {
+          ret = extInfo.Loader(fullPath, ext, extInfo);
+        }
+
+      }
+      else
+      {
+        ret = new ResponsePacket() { Error = WebServer.ServerError.UnknownType };
+      }
+
+      return ret;
+    }
+
+  }
 }
--- WebServer.cs
+++ WebServer.cs
@@ -11,181 +11,188 @@ using WebServerCSharp.Classes;
 
 namespace WebServerCSharp
 {
-    /// <summary>
-    /// Create webserver, must run VS with administration privilege to use 
-    /// </summary>
-    public class WebServer
+  /// <summary>
+  /// Create webserver, must run VS with administration privilege to use 
+  /// </summary>
+  public class WebServer
+  {
+    private static HttpListener _listener;
+    public static int maxSimultaneousConnections = 20;
+    private static Semaphore sem = new Semaphore(maxSimultaneousConnections, maxSimultaneousConnections);
+    private static Router router = new Router();
+    public static Func<ServerError, string>? onError;
+    public enum ServerError
     {
-        /// <summary>
-        /// Returns list of IP addresses assigned to localhost network devices, such as hardwired ethernet, wireless, etc.
-        /// </summary>
-        private static HttpListener _listener;
-        public static int maxSimultaneousConnections = 20;
-        private static Semaphore sem = new Semaphore(maxSimultaneousConnections, maxSimultaneousConnections);
-        private static Router router = new Router();
-        public static Func<WebServer.ServerError, string> onError;
-        public enum ServerError
-        {
-            OK,
-            ExpiredSession,
-            NotAuthorized,
-            FileNotFound,
-            PageNotFound,
-            ServerError,
-            UnknownType,
-        }
-
-        private static List<IPAddress> GetLocalHostIPs()
-        {
-            IPHostEntry host;
-            host = Dns.GetHostEntry(Dns.GetHostName());
-            List<IPAddress> ips = host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToList();
-
-            return ips;
-        }
-        private static HttpListener InitializeListener(List<IPAddress> ips)
-        {
-            HttpListener listener = new HttpListener();
-            listener.Prefixes.Add("http://localhost/");
-
-            // listen to IP address as well.
-            foreach (var ip in ips)
-            {
-                string ipAddress = $"http://{ip.ToString()}/";
-                Console.WriteLine($"Listening on IP {ipAddress}");
-                listener.Prefixes.Add(ipAddress);
-            }
-            return listener;
-        }
-
-        /// <summary>
-        /// Begin listening to connections on a separate worker thread.
-        /// </summary>
-        private static void StartListen(HttpListener listener)
-        {
-            listener.Start();
-            Task.Run(() => RunServer(listener));
-        }
-        /// <summary>
-        /// Start awaiting for connections, up to the "maxSimultaneousConnections" value.
-        /// This code runs in a separate thread.
-        /// </summary>
-        private static void RunServer(HttpListener listener)
-        {
-            while (true)
-            {
-                sem.WaitOne();
-                StartConnectionListener(listener);
-            }
-        }
-        /// <summary>
-        /// Await connections.
-        /// </summary>
-        private static async void StartConnectionListener(HttpListener listener)
-        {
-            ResponsePacket responsePacket = new ResponsePacket();
-
-            // Wait for a connection. Return to caller while we wait.
-            HttpListenerContext context = await listener.GetContextAsync();
-
-            // Release the semaphore so that another listener can be immediately started up.
-            sem.Release();
-            Log(context.Request);
-
-            // We have a connection, do something...
-
-            HttpListenerRequest request = context.Request;
-            string url = request.RawUrl;
-            string path = string.Empty;
-            string paramsString = string.Empty;
-            string verb = string.Empty; 
-            Dictionary<string, string> kvParams = new Dictionary<string, string>();
-
-            try
-            {
-                if (url.IndexOf('?') != -1)
-                {
-                path = url.Substring(0, url.IndexOf('?')); // Only the path, not any of the parameters
-                paramsString = request.RawUrl.Substring(url.IndexOf('?'), url.Length - url.IndexOf('?')); // Params on the URL itself follow the URL and are separated by a ?
-                kvParams = ConvertStringParamsToDictionary(paramsString); // Extract into key-value entries
-                }   
-                else
-                {
-                    path = url;
-                }
-
-                verb = request.HttpMethod; // get, post, delete, etc.
-
-                responsePacket = router.Route(verb, path, kvParams);
-
-                if (responsePacket.Error != ServerError.OK)
-                {
-                    //responsePacket = router.Route("GET", onError(responsePacket.Error), null);
-                    responsePacket.Redirect = onError(responsePacket.Error);
-                }
-
-                Respond(context.Request ,context.Response, responsePacket);
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
-
-
-        }
-        private static void Respond(HttpListenerRequest request, HttpListenerResponse response, ResponsePacket resp)
-        {
-            if (String.IsNullOrEmpty(resp.Redirect))
-            {
-                response.ContentType = resp.ContentType;
-                response.ContentLength64 = resp.Data.Length;
-                response.OutputStream.Write(resp.Data, 0, resp.Data.Length);
-                response.ContentEncoding = resp.Encoding;
-                response.StatusCode = (int)HttpStatusCode.OK;
-            }
-            else
-            {
-                response.StatusCode = (int)HttpStatusCode.Redirect;
-                response.Redirect("http://" + request.UserHostAddress + resp.Redirect);
-            }
-
-            response.OutputStream.Close();
-        }
-        private static Dictionary<string, string> ConvertStringParamsToDictionary(string parms)
-        {
-            string tempString = parms;
-            if (tempString[0] == '?')
-            {
-                tempString = parms.Substring(1);
-            }
-            Dictionary<string, string> dict = new Dictionary<string, string>();
-            foreach (string keyVal in tempString.Split('&'))
-            {
-                if (keyVal.Length == 2)
-                {
-                    dict.Add(keyVal.Split('=')[0], keyVal.Split("=")[1]);
-                }
-            }
-            return dict;
-        }
-
-        /// <summary>
-        /// Starts the web server.
-        /// </summary>
-        public static void Start(string websitePath)
-        {
-            router.WebsitePath = websitePath;
-            List<IPAddress> localHostIPs = GetLocalHostIPs();
-            HttpListener listener = InitializeListener(localHostIPs);
-            StartListen(listener);
-        }
-        /// <summary>
-        /// Log requests.
-        /// </summary>
-        public static void Log(HttpListenerRequest request)
-        {
-            Console.WriteLine(request.RemoteEndPoint + " " + request.HttpMethod + " /" + request.Url.AbsoluteUri);
-        }
+      OK,
+      ExpiredSession,
+      NotAuthorized,
+      FileNotFound,
+      PageNotFound,
+      ServerError,
+      UnknownType,
     }
+    /// <summary>
+    /// Starts the web server.
+    /// </summary>
+    public static void Start(string websitePath)
+    {
+      router.WebsitePath = websitePath;
+      List<IPAddress> localHostIPs = GetLocalHostIPs();
+      HttpListener listener = InitializeListener(localHostIPs);
+      listener.Start();
+
+      Task.Run(() => RunServer(listener));
+    }
+    public static void AddRoute(Route route)
+    {
+      router.AddRoute(route);
+    }
+    /// <summary>
+    /// Returns list of IP addresses assigned to localhost network devices, such as hardwired ethernet, wireless, etc.
+    /// </summary>
+    private static List<IPAddress> GetLocalHostIPs()
+    {
+      IPHostEntry host;
+      host = Dns.GetHostEntry(Dns.GetHostName());
+      List<IPAddress> ips = host.AddressList.Where(ip => ip.AddressFamily == AddressFamily.InterNetwork).ToList();
+
+      return ips;
+    }
+    private static HttpListener InitializeListener(List<IPAddress> ips)
+    {
+      HttpListener listener = new HttpListener();
+      listener.Prefixes.Add("http://localhost/");
+
+      // listen to IP address as well.
+      foreach (var ip in ips)
+      {
+        string ipAddress = $"http://{ip.ToString()}/";
+        Console.WriteLine($"Listening on IP {ipAddress}");
+        listener.Prefixes.Add(ipAddress);
+      }
+      return listener;
+    }
+
+    /// <summary>
+    /// Start awaiting for connections, up to the "maxSimultaneousConnections" value.
+    /// This code runs in a(one) separate thread.
+    /// </summary>
+    private static void RunServer(HttpListener listener)
+    {
+      while (true)
+      {
+        sem.WaitOne();
+        StartConnectionListener(listener);
+      }
+    }
+
+    /// <summary>
+    /// Await connections.
+    /// </summary>
+    private static async void StartConnectionListener(HttpListener listener)
+    {
+      ResponsePacket responsePacket = new ResponsePacket();
+
+      // Wait for a connection. Return to caller while we wait.
+      HttpListenerContext context = await listener.GetContextAsync();
+
+      // Release the semaphore so that another listener can be immediately started up.
+      sem.Release();
+      Log(context.Request);
+
+      // We have a connection, do something...
+
+      HttpListenerRequest request = context.Request;
+      string url = request.RawUrl;
+      string path = string.Empty;
+      string paramsString = string.Empty;
+      string verb = string.Empty;
+      Dictionary<string, string> kvParams = new Dictionary<string, string>();
+
+      try
+      {
+        if (url.IndexOf('?') != -1)
+        {
+          path = url.Substring(0, url.IndexOf('?')); // Only the path, not any of the parameters
+          paramsString = request.RawUrl.Substring(url.IndexOf('?'), url.Length - url.IndexOf('?')); // Params on the URL itself follow the URL and are separated by a ?
+          kvParams = ConvertStringParamsToDictionary(paramsString); // Extract into key-value entries
+          Log(kvParams);
+        }
+        else
+        {
+          path = url;
+        }
+
+        verb = request.HttpMethod; // get, post, delete, etc.
+
+        responsePacket = router.Route(verb, path, kvParams);
+
+        if (responsePacket.Error != ServerError.OK)
+        {
+          //responsePacket = router.Route("GET", onError(responsePacket.Error), null);
+          responsePacket.Redirect = onError(responsePacket.Error);
+        }
+
+        Respond(context.Request, context.Response, responsePacket);
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        Console.WriteLine(e.StackTrace);
+        responsePacket = new ResponsePacket() { Redirect = onError(ServerError.ServerError) };
+      }
+
+
+    }
+    private static void Respond(HttpListenerRequest request, HttpListenerResponse response, ResponsePacket responsePacket)
+    {
+      if (String.IsNullOrEmpty(responsePacket.Redirect))
+      {
+        response.ContentType = responsePacket.ContentType;
+        response.ContentLength64 = responsePacket.Data.Length;
+        response.OutputStream.Write(responsePacket.Data, 0, responsePacket.Data.Length);
+        response.ContentEncoding = responsePacket.Encoding;
+        response.StatusCode = (int)HttpStatusCode.OK;
+      }
+      else
+      {
+        response.StatusCode = (int)HttpStatusCode.Redirect;
+        response.Redirect("http://" + request.UserHostAddress + responsePacket.Redirect);
+      }
+
+      response.OutputStream.Close();
+    }
+    private static Dictionary<string, string> ConvertStringParamsToDictionary(string paramStr)
+    {
+      string tempString = paramStr;
+      if (tempString[0] == '?')
+      {
+        tempString = paramStr.Substring(1);
+      }
+      Dictionary<string, string> dict = new Dictionary<string, string>();
+      foreach (string keyVal in tempString.Split('&'))
+      {
+        if (keyVal.Length == 2)
+        {
+          dict.Add(keyVal.Split('=')[0], keyVal.Split("=")[1]);
+        }
+      }
+      return dict;
+    }
+
+    /// <summary>
+    /// Log requests.
+    /// </summary>
+    public static void Log(HttpListenerRequest request)
+    {
+      Console.WriteLine(request.RemoteEndPoint + " " + request.HttpMethod + " /" + request.Url.AbsoluteUri);
+    }
+    private static void Log(Dictionary<string, string> kv)
+    {
+      foreach (var item in kv)
+      {
+        Console.WriteLine(item.Key + " : " + item.Value);
+      }
+    }
+  }
 }
